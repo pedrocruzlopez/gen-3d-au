89194838d2b32f91766724f5d04909fc5b05d81f
Alina Urbina <alin2525@gmail.com>
2017-10-05 00:15:28 -0600

    #4 creacion de las clases para empezar a crear diagramas

---
+++ Proyecto2_201122872/Proyecto2_201122872/UML/Atributo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto2_201122872.UML
+{
+    class Atributo
+    {
+        private string visibilidad;
+        private string nombre;
+        private string tipo;
+
+
+        public Atributo(string visibiliad, string nombre, string tipo)
+        {
+            this.visibilidad = visibiliad;
+            this.nombre = nombre;
+            this.tipo = tipo;
+        }
+
+
+
+
+    }
+}
