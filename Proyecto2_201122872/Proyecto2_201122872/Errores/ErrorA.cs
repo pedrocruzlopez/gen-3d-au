8556d0bd574b8f1c6a7b4b5dc5acb9b40e36f116
Alina Urbina <alin2525@gmail.com>
2017-10-11 01:58:03 -0600

    #17 Creacion edd para errores lexicos, sintacitcos y semanticos

---
+++ Proyecto2_201122872/Proyecto2_201122872/Errores/ErrorA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto2_201122872.Errores
+{
+    public class ErrorA
+    {
+        public string tipoError;
+        public int fila;
+        public int posicion;
+        public int columna;
+        public string mensaje;
+
+
+        public ErrorA(string tipo, int fila, int pos, int col, string mensaje)
+        {
+            this.tipoError = tipo;
+            this.fila = fila;
+            this.posicion = pos;
+            this.columna = col;
+            this.mensaje = mensaje;
+        }
+
+
+
+    }
+}
---
+++ Proyecto2_201122872/Proyecto2_201122872/Errores/ListaErrores.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto2_201122872.Errores
+{
+    public class ListaErrores
+    {
+        public List<ErrorA> errores;
+
+
+        public ListaErrores()
+        {
+            this.errores = new List<ErrorA>();
+        }
+
+
+        public void addError(ErrorA nuevo){
+            this.errores.Add(nuevo);
+        }
+
+
+    }
+}
