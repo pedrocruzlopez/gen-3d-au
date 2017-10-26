d580e15fbb50ba3ce1bc14a7143a1163c3cce797
Alina Urbina <alin2525@gmail.com>
2017-10-26 02:08:31 -0600

    #48 Creacion de estructura para guardar temporales

--- Proyecto2_201122872/Proyecto2_201122872/Interprete3D/Ejecucion3D/ListaTemporales.cs
+++ Proyecto2_201122872/Proyecto2_201122872/Interprete3D/Ejecucion3D/ListaTemporales.cs
@@ -8,5 +8,79 @@ namespace Proyecto2_201122872.Interprete3D.Ejecucion3D
 {
     public class ListaTemporales
     {
+        public List<Temporal> listaTemporales;
+
+        public ListaTemporales()
+        {
+            this.listaTemporales = new List<Temporal>();
+        }
+
+
+        public int getSize()
+        {
+            return this.listaTemporales.Count;
+        }
+
+
+        public object getValorTemp(String nombre)
+        {
+            foreach (Temporal item in this.listaTemporales)
+            {
+                if (item.nombre.Equals(nombre, StringComparison.OrdinalIgnoreCase))
+                {
+                    return item.valor;
+                }
+            }
+            return null;
+        }
+
+        public Boolean exiteTemp(Temporal temp)
+        {
+            foreach (Temporal item in listaTemporales)
+            {
+                if (item.nombre.Equals(temp.nombre, StringComparison.OrdinalIgnoreCase))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        public void agregarTemp(Temporal temp)
+        {
+            if (getSize() > 0)
+            {
+                if (!exiteTemp(temp))
+                {
+                    listaTemporales.Add(temp);
+                }else{
+                    modificarTemp(temp);
+                }
+
+            }
+            else
+            {
+                listaTemporales.Add(temp);
+            }
+        }
+
+        public void modificarTemp(Temporal temp)
+        {
+            if (exiteTemp(temp))
+            {
+                foreach (Temporal item in this.listaTemporales)
+                {
+                    if (item.nombre.Equals(temp.nombre, StringComparison.OrdinalIgnoreCase))
+                    {
+                        item.valor = temp.valor;
+                        break;
+                    }
+                }
+            }
+        }
+
+
     }
 }
